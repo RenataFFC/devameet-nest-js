3b146721db0c895ec4a71a37ba35a07cfe3e6e80
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-05-27 20:20:05 -0300

    Devammet-Nestjs - Login

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.services";
+
+
+@Module({
+  imports:[],
+  controllers:[],
+  providers:[]
+
+})
+export class AuthModule {}
