67f10cd90802b9719011cae37b95dbcebe0a80ff
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-06-05 13:43:14 -0300

    Register

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,21 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from 'passport-jwt';
-
-
+import { Strategy, ExtractJwt} from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy( Strategy){
-  constructor(){
-     super({
-        jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
-        ignoreExpiration:true,
-        secretOrkey: process.env.USER_JWT_SECRET_KEY,
-     });
-  }
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: true,
+            secretOrKey: process.env.USER_JWT_SECRET_KEY,
+        });
+    }
 
-  async validate(payload:any){
-    return {userId:payload.sub, email:payload.email};
-  } 
-
+    async validate(payload:any){
+        return {userId: payload.sub, email: payload.email};
+    }
 }
