25af627d4ce9a61b63ce8391377a4931fa38f3eb
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-06-09 17:13:04 -0300

    Get , Delete e Post do Meet

--- src/meet/meet.controller.ts
+++ src/meet/meet.controller.ts
@@ -5,35 +5,35 @@ import { MeetService } from './meet.service';
 
 @Controller('meet') 
 export class MeetController { 
-constructor( 
-private readonly service: MeetService) { } 
+      constructor( 
+          private readonly service: MeetService
+          ){} 
 
 @Get() 
-async getUser(@Request() req) { 
-const { userId } = req?.user; 
-const result = await this.service.getMeetsByUser(userId); 
+async getUser(@Request() req){ 
+        const { userId } = req?.user; 
 
-return result?.map((m) => ({ 
-id: m._id.toString(), 
-name: m.name, 
-color: m.color, 
-link: m.link, 
-}) as GetMeetDto); 
-} 
+        const result = await this.service.getMeetsByUser(userId); 
+
+        return result?.map((m) => ({ 
+        id: m._id.toString(), 
+        name: m.name, 
+        color: m.color, 
+        link: m.link, 
+        }) as GetMeetDto); 
+        } 
 
 @Post() 
-@HttpCode(HttpStatus.OK) 
-async create(@Request() req, @Body() dto: CreateMeetDto) { 
+async createMeet(@Request() req, @Body() dto: CreateMeetDto) { 
 const { userId } = req?.user; 
-await this.service.create(userId, dto); 
+await this.service.createMeet(userId, dto); 
 } 
 
 @Delete(':id') 
-@HttpCode(HttpStatus.NO_CONTENT) 
-async deleteMeet(@Param() params, @Request() req) { 
+async deleteMeet(@Request() req,@Param() params) { 
 const { userId } = req?.user; 
 const { id } = params; 
-await this.service.delete(userId, id); 
+await this.service.deleteMeetByUser(userId, id); 
 } 
 }
 
--- src/meet/meet.service.ts
+++ src/meet/meet.service.ts
@@ -4,37 +4,38 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose'; 
 import { CreateMeetDto } from './dtos/createmeet.dto'; 
 import { UserService } from 'src/user/user.service'; 
-
+import { generateLink } from './helpers/linkgenerator.helper';
 
 @Injectable() 
 export class MeetService { 
-private logger = new Logger(MeetService.name); 
+      private logger = new Logger(MeetService.name); 
 
-constructor( 
-@InjectModel(Meet.name) private model: Model<MeetDocument>, 
-private readonly userService : UserService) { } 
+       constructor( 
+              @InjectModel(Meet.name) private model: Model<MeetDocument>, 
+              private readonly userService : UserService
+              ){} 
 
-async getMeetsByUser(userId: string) { 
-this.logger.debug('getMeetsByUser - start'); 
-return await this.model.find({ user: userId }); 
-} 
+              async getMeetsByUser(userId: string) { 
+                this.logger.debug('getMeetsByUser - ' + userId); 
+                return await this.model.find({ user: userId }); 
+              } 
 
-async create(userId: string, dto: CreateMeetDto) { 
-this.logger.debug('create - start'); 
+              async createMeet(userId: string, dto: CreateMeetDto) { 
+                this.logger.debug('createMeet - ' + userId); 
 
-const user = await this.userService.getUserById(userId); 
-const payload = { 
-...dto, 
-user, 
- 	    link : generateLink() 
-}; 
-this.logger.debug('create - payload before save', payload); 
+              const user = await this.userService.getUserById(userId); 
+              const meet = { 
+              ...dto, 
+              user, 
+              link: generateLink() 
+              }; 
 
-const createdUser = new this.model(payload); 
-return createdUser.save(); 
-} 
-async delete(userId: string, meetId: string) { 
-this.logger.debug('delete - start'); 
-return await this.model.deleteOne({ user: userId, _id: meetId }); 
-} 
+              const createMeet = new this.model(meet);
+              return await createMeet.save();
+          } 
+          
+            async deleteMeetByUser(userId: string, meetId: string) { 
+            this.logger.debug(`deleteMeetByUser - ${userId} - ${meetId}` );
+            return await this.model.deleteOne({ user: userId, _id: meetId }); 
+            } 
 }
